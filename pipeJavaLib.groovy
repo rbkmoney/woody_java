59624f61380be75a1cd63ffac033046fbe0252b9
a.semenkov <sealexey@gmail.com>
2016-09-06 14:29:37 +0300

    + refactor a lttle

--- pipeJavaLib.groovy
+++ pipeJavaLib.groovy
@@ -1,5 +1,8 @@
 //Default pipeline for Java library
-def call(String buildImageTag, Closure body) {
+def call(String buildImageTag, String mvnArgs = "") {
+
+    // Arguments for mvn install in build container. For exmple: ' -DjvmArgs="-Xmx256m" '
+    env.MVN_ARGS = mvnArgs
 
     def buildContainer = docker.image("rbkmoney/build:${buildImageTag}")
     runStage('Pull build container') {
@@ -10,19 +13,16 @@ def call(String buildImageTag, Closure body) {
     }
 
     runStage('Execute build container') {
-        docker.withRegistry('https://dr.rbkmoney.com/v2/', 'dockerhub-rbkmoneycibot') {
-            withCredentials([[$class: 'FileBinding', credentialsId: 'java-maven-settings.xml', variable: 'SETTINGS_XML']]) {
-                buildContainer.inside() {
-                    if (env.BRANCH_NAME == 'master') {
-                        sh 'mvn deploy --batch-mode --settings  $SETTINGS_XML'
-                    } else {
-                        sh 'mvn install --batch-mode --settings  $SETTINGS_XML'
-                    }
+        withCredentials([[$class: 'FileBinding', credentialsId: 'java-maven-settings.xml', variable: 'SETTINGS_XML']]) {
+            buildContainer.inside() {
+                if (env.BRANCH_NAME == 'master') {
+                    sh 'mvn deploy --batch-mode --settings  $SETTINGS_XML $MVN_ARGS'
+                } else {
+                    sh 'mvn install --batch-mode --settings  $SETTINGS_XML $MVN_ARGS'
                 }
             }
         }
     }
-
-    body.call()
 }
+
 return this
