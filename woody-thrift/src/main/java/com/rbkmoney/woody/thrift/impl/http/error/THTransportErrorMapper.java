92ebb091416750abfd17ed8694910f13be117ec9
Vladimir Pankrashkin <v.pankrashkin@rbkmoney.com>
2017-12-24 19:58:38 +0300

    BJ-259: Add transport error mapping for java.net.ConnectException -> unavailable, java.net.* -> undefied

--- woody-thrift/src/main/java/com/rbkmoney/woody/thrift/impl/http/error/THTransportErrorMapper.java
+++ woody-thrift/src/main/java/com/rbkmoney/woody/thrift/impl/http/error/THTransportErrorMapper.java
@@ -11,23 +11,27 @@ import java.util.regex.Pattern;
 
 public class THTransportErrorMapper implements WErrorMapper {
     private static final ErrorAnalyzer[] analyzers = new ErrorAnalyzer[]{
-            new ErrorAnalyzer(Pattern.compile(SocketTimeoutException.class.getName()), (t, c) -> {
-                WErrorDefinition def = new WErrorDefinition(WErrorSource.EXTERNAL);
-                def.setErrorSource(WErrorSource.INTERNAL);
-                def.setErrorType(WErrorType.UNDEFINED_RESULT);
-                def.setErrorReason(t.toString());
-                def.setErrorMessage(t.getMessage());
-                return def;
-            }),
-            new ErrorAnalyzer(Pattern.compile("java.net.Socket.*"), THTransportErrorMapper::genUnavailableResult),
-            new ErrorAnalyzer(Pattern.compile(org.apache.http.NoHttpResponseException.class.getName()), THTransportErrorMapper::genUnavailableResult),
-            new ErrorAnalyzer(Pattern.compile(java.net.UnknownHostException.class.getName()), THTransportErrorMapper::genUnavailableResult),
+            new ErrorAnalyzer(Pattern.compile(SocketTimeoutException.class.getName()), THTransportErrorMapper::getUndefinedResult),
+            new ErrorAnalyzer(Pattern.compile(Pattern.quote(org.apache.http.NoHttpResponseException.class.getName())), THTransportErrorMapper::genUnavailableResult),
+            new ErrorAnalyzer(Pattern.compile(Pattern.quote(java.net.UnknownHostException.class.getName())), THTransportErrorMapper::genUnavailableResult),
+            new ErrorAnalyzer(Pattern.compile(Pattern.quote(java.net.ConnectException.class.getName())), THTransportErrorMapper::genUnavailableResult),
+            new ErrorAnalyzer(Pattern.compile("java\\.net\\.Socket\\..*"), THTransportErrorMapper::genUnavailableResult),
+            new ErrorAnalyzer(Pattern.compile("java\\.net\\..*"), THTransportErrorMapper::getUndefinedResult),
     };
 
     private static WErrorDefinition genUnavailableResult(Throwable t, ContextSpan c) {
         WErrorDefinition def = new WErrorDefinition(WErrorSource.EXTERNAL);
         def.setErrorSource(WErrorSource.INTERNAL);
         def.setErrorType(WErrorType.UNAVAILABLE_RESULT);
+        def.setErrorReason(t.toString());
+        def.setErrorMessage(t.getMessage());
+        return def;
+    }
+
+    private static WErrorDefinition getUndefinedResult(Throwable t, ContextSpan c) {
+        WErrorDefinition def = new WErrorDefinition(WErrorSource.EXTERNAL);
+        def.setErrorSource(WErrorSource.INTERNAL);
+        def.setErrorType(WErrorType.UNDEFINED_RESULT);
         def.setErrorReason(t.toString());
         def.setErrorMessage(t.getMessage());
         return def;
